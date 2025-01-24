8f7009cc39ba493df64a4d6785e559709a019dd1
raffael-marinho <silvamarinho10@gmail.com>
2025-01-24 07:03:13 -0300

    feat: adiciona o construtor na classe paciente

--- prontuario/src/programa/app/Paciente.java
+++ prontuario/src/programa/app/Paciente.java
@@ -1,5 +1,20 @@
 package programa.app;
 
 public class Paciente {
-
+	
+	String cpf;
+	String nome;
+	float peso;
+	float altura;
+	int idade;
+	
+	public Paciente(String cpf, String nome, float peso, float altura, int idade) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.peso = peso;
+		this.altura = altura;
+		this.idade = idade;
+	}
+	
+	
 }
