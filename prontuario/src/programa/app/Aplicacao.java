dba133d5284b17717e8ebe96135cc0e9b7833ecd
raffael-marinho <silvamarinho10@gmail.com>
2025-01-24 11:42:20 -0300

    feat: add a sub-classe pacienteFeminino e refenciando paciente como super-classe

--- prontuario/src/programa/app/Aplicacao.java
+++ prontuario/src/programa/app/Aplicacao.java
@@ -6,6 +6,9 @@ public class Aplicacao {
         Paciente p1 = new Paciente("123.456.789-00", "João Silva", 75.5f, 1.78f, 29, 'M');
 		
 		System.out.println(p1);
+        System.out.println("IMC: " + p1.calcularImc());
+        System.out.println("Metabolismo Basal: " + p1.calcularMetabolismoBasal());
+
 	}
 
 }
---
+++ prontuario/src/programa/app/PacienteFeminino.java
@@ -0,0 +1,10 @@
+package programa.app;
+
+public class PacienteFeminino extends Paciente {
+
+	public PacienteFeminino(String cpf, String nome, float peso, float altura, int idade, char sexo) {
+		super(cpf, nome, peso, altura, idade, sexo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
