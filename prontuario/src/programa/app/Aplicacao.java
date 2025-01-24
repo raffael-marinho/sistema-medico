451d16b588334a57f1576285a6fcb8daf68c7716
raffael-marinho <silvamarinho10@gmail.com>
2025-01-24 10:48:07 -0300

    feat: add a classe Aplicacao e imprime paciente p1

---
+++ prontuario/src/programa/app/Aplicacao.java
@@ -0,0 +1,11 @@
+package programa.app;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+        Paciente p1 = new Paciente("123.456.789-00", "João Silva", 75.5f, 1.78f, 29, 'M');
+		
+		System.out.println(p1);
+	}
+
+}
